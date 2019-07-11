f48368c0262f44e76271df5192cad123a67d23c8
huzghost@126.com <huzghost@126.com>
2019-07-11 08:18:32 +0800

    1，移植s3c2440的串口，目前可以使用串口

--- cpu/arm920t/s3c24x0/speed.c
+++ cpu/arm920t/s3c24x0/speed.c
@@ -38,6 +38,8 @@
 #include <s3c2410.h>
 #endif
 
+DECLARE_GLOBAL_DATA_PTR;
+
 #define MPLL 0
 #define UPLL 1
 
@@ -57,17 +59,20 @@ static ulong get_PLLCLK(int pllreg)
     ulong r, m, p, s;
 
     if (pllreg == MPLL)
-	r = clk_power->MPLLCON;
+		r = clk_power->MPLLCON;										//clk_power->MPLLCON的赋值在board_init()初如化函数实现
     else if (pllreg == UPLL)
-	r = clk_power->UPLLCON;
+		r = clk_power->UPLLCON;										//clk_power->UPLLCON的赋值在board_init()初如化函数实现
     else
-	hang();
+		hang();
 
-    m = ((r & 0xFF000) >> 12) + 8;
-    p = ((r & 0x003F0) >> 4) + 2;
-    s = r & 0x3;
+    m = ((r & 0xFF000) >> 12) + 8;								//计算出 m (MDIV+8)
+    p = ((r & 0x003F0) >> 4) + 2;								//计算出 p (PDIV+2)
+    s = r & 0x3;												//计算出 s (SDIV)
 
-    return((CONFIG_SYS_CLK_FREQ * m) / (p << s));
+	//这是算出 2410的PLLCLK，我们是2440 ，这里需要修改
+    //return((CONFIG_SYS_CLK_FREQ * m) / (p << s));				//得到 MPLLCON 或 UPLLCON，得到系统运行时钟
+
+    return ((CONFIG_SYS_CLK_FREQ * m * 2) / (p << s));			//得到s3c2440的时钟
 }
 
 /* return FCLK frequency */
@@ -81,7 +86,9 @@ ulong get_HCLK(void)
 {
     S3C24X0_CLOCK_POWER * const clk_power = S3C24X0_GetBase_CLOCK_POWER();
 
-    return((clk_power->CLKDIVN & 0x2) ? get_FCLK()/2 : get_FCLK());
+    //return((clk_power->CLKDIVN & 0x2) ? get_FCLK()/2 : get_FCLK());	//是否为系统时钟的1/2
+
+	return ((clk_power->CLKDIVN & 0x4) ? get_FCLK()/4 : get_FCLK());
 }
 
 /* return PCLK frequency */
@@ -89,7 +96,9 @@ ulong get_PCLK(void)
 {
     S3C24X0_CLOCK_POWER * const clk_power = S3C24X0_GetBase_CLOCK_POWER();
 
-    return((clk_power->CLKDIVN & 0x1) ? get_HCLK()/2 : get_HCLK());
+    //return((clk_power->CLKDIVN & 0x1) ? get_HCLK()/2 : get_HCLK());//是否为HCLK的1/2
+
+	return((clk_power->CLKDIVN & 0x1) ? get_HCLK()/2 : get_HCLK());//是否为HCLK的1/2
 }
 
 /* return UCLK frequency */
--- include/s3c24x0.h
+++ include/s3c24x0.h
@@ -126,6 +126,7 @@ typedef struct {
 	S3C24X0_REG32	CLKCON;
 	S3C24X0_REG32	CLKSLOW;
 	S3C24X0_REG32	CLKDIVN;
+	S3C24X0_REG32   CAMDIVN;    /* for s3c2440, by huzghost@126.com */
 } /*__attribute__((__packed__))*/ S3C24X0_CLOCK_POWER;
 
 
@@ -164,6 +165,25 @@ typedef struct {
 	S3C24X0_REG32	NFECC;
 } /*__attribute__((__packed__))*/ S3C2410_NAND;
 
+/* NAND FLASH (see S3C2440 manual chapter 6, huzghost@126.com) */
+typedef struct {
+    S3C24X0_REG32   NFCONF;
+    S3C24X0_REG32   NFCONT;
+    S3C24X0_REG32   NFCMD;
+    S3C24X0_REG32   NFADDR;
+    S3C24X0_REG32   NFDATA;
+    S3C24X0_REG32   NFMECCD0;
+    S3C24X0_REG32   NFMECCD1;
+    S3C24X0_REG32   NFSECCD;
+    S3C24X0_REG32   NFSTAT;
+    S3C24X0_REG32   NFESTAT0;
+    S3C24X0_REG32   NFESTAT1;
+    S3C24X0_REG32   NFMECC0;
+    S3C24X0_REG32   NFMECC1;
+    S3C24X0_REG32   NFSECC;
+    S3C24X0_REG32   NFSBLK;
+    S3C24X0_REG32   NFEBLK;
+} /*__attribute__((__packed__))*/ S3C2440_NAND;
 
 /* UART (see manual chapter 11) */
 typedef struct {
@@ -450,6 +470,14 @@ typedef struct {
 	S3C24X0_REG32	GSTATUS2;
 	S3C24X0_REG32	GSTATUS3;
 	S3C24X0_REG32	GSTATUS4;
+
+	/* s3c2440 */
+    S3C24X0_REG32   res9[3];
+	S3C24X0_REG32	MSLCON;
+    S3C24X0_REG32   GPJCON;
+    S3C24X0_REG32   GPJDAT;
+    S3C24X0_REG32   GPJUP;
+	
 #endif
 } /*__attribute__((__packed__))*/ S3C24X0_GPIO;
 
